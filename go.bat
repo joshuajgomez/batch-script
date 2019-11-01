44c565ac8c9b1f60b6123484342998a4b85fcbce
Joshua Gomez <joshua.gomez@quest-global.com>
2019-11-01 16:39:40 +0900

    v1.0.0

--- go.bat
+++ go.bat
@@ -1,11 +1,67 @@
-SET source=C:\Users\1020593\source\
+@ECHO OFF
 
-SET dm=vendor\quest\Phone\DeviceManagerService\
-SET ph=vendor\quest\Phone\PhoneHMI\
-SET ps=vendor\quest\Phone\PhoneService\
+REM Creating a Newline variable (the two blank lines are required!)
+
+SET source=C:\Users\1020593\source\vendor\
+
+SET dm=quest\Phone\DeviceManagerService\
+SET ph=quest\Phone\PhoneHMI\
+SET ps=quest\Phone\PhoneService\
+
+SET option=%1
 
 SET dest=%source%%dm%
 
-ECHO %dest%
+:: If no arguments, proceed with default option
+IF [%option%]==[] GOTO DEFAULT
+	 
+IF %option%==dm (
+	:: Goto DeviceManagerService
+	SET dest=%source%%dm%
+) ELSE (
+	IF %option%==ph (
+		:: Goto PhoneHMI
+		SET dest=%source%%ph%
+	) ELSE (
+		IF %option%==ps (
+			:: Goto PhoneService
+			SET dest=%source%%ps%
+		) ELSE (
+			:: Invalid arguments
+			ECHO Invalid argument: %option%
+			@ECHO:
+			ECHO To navigate to project folders, use: go ^<option^>
+			@ECHO:
+			ECHO Available destinations are:
+			@ECHO:
+			ECHO dm 	 %dm%
+			ECHO ph 	 %ph%
+			ECHO ps 	 %ps%
+			@ECHO:
+			GOTO EXIT
+		)
+	)
+)
 
+
+:DEFAULT
+ECHO Going to %dest%
+@ECHO:
 CD %dest%
+
+
+:EXIT
+
+
+
+
+
+
+
+
+
+
+
+
+
+

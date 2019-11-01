d68b87be57592fd20cf89a685d6acee0f71c93e4
Joshua Gomez <joshua.gomez@quest-global.com>
2019-11-01 15:12:35 +0900

    Initial commit

---
+++ go.bat
@@ -0,0 +1,11 @@
+SET source=C:\Users\1020593\source\
+
+SET dm=vendor\quest\Phone\DeviceManagerService\
+SET ph=vendor\quest\Phone\PhoneHMI\
+SET ps=vendor\quest\Phone\PhoneService\
+
+SET dest=%source%%dm%
+
+ECHO %dest%
+
+CD %dest%

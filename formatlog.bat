9ffd4aa159e5ed5f474461c247ebb98b73652f63
Joshua Gomez <joshua.gomez@quest-global.com>
2019-11-08 19:46:19 +0900

    Add help

--- formatlog.bat
+++ formatlog.bat
@@ -1,2 +1,13 @@
+@echo off
+
+set jarFilePath="C:\Users\1020593\batch-script\formatLog.jar"
+
+
+@echo.
+echo =============================
+echo formatlog - LogFormatter tool
+echo =============================
+@echo.
+
 :: Execute formatLog.jar
-java -jar formatLog.jar %1
+java -jar %jarFilePath% %1

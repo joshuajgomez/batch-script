9b8c1afc8cf6d61f645397b8708913a60f564441
Joshua Gomez <joshua.gomez@quest-global.com>
2019-11-11 13:18:29 +0900

    Paste logs to open editor

--- formatlog.bat
+++ formatlog.bat
@@ -1,13 +1,32 @@
 @echo off
 
+:: Define constants
 set jarFilePath="C:\Users\1020593\batch-script\formatLog.jar"
+set inputFile="input_logs.txt"
+set textEditor="C:\Program Files\Notepad++\notepad++.exe"
 
-
+:: Print info
 @echo.
 echo =============================
 echo formatlog - LogFormatter tool
 echo =============================
 @echo.
 
+:: Clear input file
+break > %inputFile%
+
+:: Open text editor for getting logs
+%textEditor% %inputFile%
+
+:: Wait for input 
+set /p id=Paste log to editor and save it, press any key to continue...
+
+@echo.
+echo Formatted log:
+@echo.
+
 :: Execute formatLog.jar
-java -jar %jarFilePath% %1
+java -jar %jarFilePath% %inputFile%
+
+:: Delete input file
+del %inputFile% /s /f /q >nul

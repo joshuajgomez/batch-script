b67b607b1da9cac9cca9967664bf3c0479346314
Joshua Gomez <joshua.gomez@quest-global.com>
2019-11-11 15:04:26 +0900

    Use notepad as text editor

--- logf.bat
+++ logf.bat
@@ -2,24 +2,23 @@
 
 :: Define constants
 set jarFilePath="C:\Users\1020593\batch-script\logf-bin.jar"
-set inputFile="input_logs.txt"
-set textEditor="C:\Program Files\Notepad++\notepad++.exe"
+set inputFile="C:\Users\1020593\batch-script\input_logs.txt"
+set textEditor="C:\Windows\system32\notepad.exe"
 
 :: Print info
 @echo.
-echo =============================
-echo formatlog - LogFormatter tool
-echo =============================
+echo ========================
+echo logf - LogFormatter tool
+echo ========================
 @echo.
+
+echo Paste logs to file, then save and close...
 
 :: Clear input file
 break > %inputFile%
 
 :: Open text editor for getting logs
 %textEditor% %inputFile%
-
-:: Wait for input 
-set /p id=Paste log to editor and save it, press any key to continue...
 
 @echo.
 echo Formatted log:
